65c4855b3fa0c1ba8e40b67aa703b22aea449769
cindrolino <marko.cindric7@gmail.com>
2024-05-11 19:02:57 +0200

    Revert "Initial code"
    This reverts commit 9d795ab081d83d0825fee206da7d4cf4dc470b43.

--- src/main/java/com/Craftify/Craftify/models/News.java
+++ src/main/java/com/Craftify/Craftify/models/News.java
@@ -2,17 +2,4 @@ package com.Craftify.Craftify.models;
 import jakarta.persistance.*;
 
 public class News {
-    private String title;
-    private String description;
-    private String author;
-    private String url;
-    private String date;
-    public News(String title, String description, String author, String url, String date) {
-        this.title = title;
-        this.description = description;
-        this.author = author;
-        this.url = url;
-        this.date = date;
-    }
-
 }
